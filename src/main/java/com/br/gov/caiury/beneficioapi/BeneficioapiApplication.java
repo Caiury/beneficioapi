80dbd07e41e4637c7d32628f88531750033b571a
Caiury <caiuryf@gmail.com>
2024-03-12 20:53:18 -0300

    first commit

---
+++ src/main/java/com/br/gov/caiury/beneficioapi/BeneficioapiApplication.java
@@ -0,0 +1,13 @@
+package com.br.gov.caiury.beneficioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficioapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gov/caiury/beneficioapi/BeneficioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gov.caiury.beneficioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
